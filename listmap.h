aace75993afefc8d1403eeb5393480e0d9d6f27e
Bettie Jea <bjea@ucsc.edu>
2016-02-07 11:37:28 -0800

    2nd Commit, All Files Added

---
+++ listmap.h
@@ -0,0 +1,64 @@
+// $Id: listmap.h,v 1.11 2015-04-28 19:22:02-07 - - $
+
+#ifndef __LISTMAP_H__
+#define __LISTMAP_H__
+
+#include "xless.h"
+#include "xpair.h"
+
+// We have no control over Key or Value
+template <typename Key, typename Value, class Less=xless<Key>>  // We specify default value of Less here, but not in the implementation file.
+class listmap {
+   public:
+      using key_type = Key;
+      using mapped_type = Value;
+      using value_type = xpair<const key_type, mapped_type>;
+   private:
+      Less less;    // We need constructors for link, ?
+      struct node;
+      struct link {
+         node* next{};
+         node* prev{};
+         link (node* next, node* prev): next(next), prev(prev){}
+      };
+      struct node: link {
+         value_type value{};
+         node (node* next, node* prev, const value_type&);
+      };
+      node* anchor() { return static_cast<node*> (&anchor_); }
+      link anchor_ {anchor(), anchor()};
+   public:
+      class iterator;
+      listmap(){};
+      listmap (const listmap&);
+      listmap& operator= (const listmap&);
+      ~listmap();   // Look at the example in xlist.h: ~xlist(){ if (not empty()) pop_back();}
+      iterator insert (const value_type&);
+      iterator find (const key_type&) const;
+      iterator erase (iterator position);
+      iterator begin() { return anchor()->next; }
+      iterator end() { return anchor(); }
+      bool empty() const { return begin() == end(); }
+};
+
+
+template <typename Key, typename Value, class Less=xless<Key>>
+class listmap<Key,Value,Less>::iterator {
+   private:
+      friend class listmap<Key,Value>;
+      listmap<Key,Value,Less>::node* where {nullptr};
+      iterator (node* where): where(where){};
+   public:
+      iterator(){}
+      value_type& operator*();
+      value_type* operator->(); // Unary operator, p->f, actually is p(->f), binding to f, i.operator->()
+      iterator& operator++(); //++itor
+      iterator& operator--(); //--itor
+      void erase();
+      bool operator== (const iterator&) const;
+      bool operator!= (const iterator&) const;
+};
+
+#include "listmap.tcc"
+#endif
+
